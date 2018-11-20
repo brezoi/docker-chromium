f7200758377d3a8dfe408e7d63588c5e6672cf95
brezoi <brezoi@users.noreply.github.com>
2018-11-20 14:56:35 +0000

    Add --rm to launch.sh

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
 # allow X11 forwarding permission
 xhost +local:"$USER"
 
-docker run --device /dev/snd -e DISPLAY=unix"$DISPLAY" --volume /tmp/.X11-unix:/tmp/.X11-unix  --volume /dev/shm:/dev/shm -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native -v ~/.config/pulse/cookie:/root/.config/pulse/cookie --group-add $(getent group audio | cut -d: -f3) --security-opt seccomp=seccomp.json local/chromium "$@"
+docker run --rm --device /dev/snd -e DISPLAY=unix"$DISPLAY" --volume /tmp/.X11-unix:/tmp/.X11-unix  --volume /dev/shm:/dev/shm -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native -v ~/.config/pulse/cookie:/root/.config/pulse/cookie --group-add $(getent group audio | cut -d: -f3) --security-opt seccomp=seccomp.json local/chromium "$@"
